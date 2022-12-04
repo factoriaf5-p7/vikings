e774b18a3bcebf7a6b7983242d10cdf359f34cfd
Raul <you@example.com>
2022-12-04 12:25:02 +0100

    code-starter ready

--- src/saxon/Saxon.ts
+++ src/saxon/Saxon.ts
@@ -1,18 +1,3 @@
-import { Soldier } from "../soldier/Soldier";
+export class Saxon {
 
-export class Saxon extends Soldier {
-    constructor(health: number, strength: number) {
-        super(health, strength)
-    }
-    receiveDamage(damage: number): string {
-        super.receiveDamage(damage);
-        
-
-        if (this.health > 0) {
-            return `A Saxon has received ${damage} points of damage`
-        } else {
-            return `A Saxon has died in combat`
-        }
-
-    }
 }
