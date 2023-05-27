a3cf38f140a38fd1e2b23223be24873d67a65171
Angela-GM <angelagm0112@gmail.com>
2023-05-27 08:22:16 +0200

    complete soldier and viking

--- src/soldier/Soldier.ts
+++ src/soldier/Soldier.ts
@@ -1,2 +1,23 @@
 export class Soldier {
+    strength: number;
+    health: number;
+    
+
+    // Constructor 
+    constructor(health: number, strength: number) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack = () => { return this.strength}
+    receiveDamage = (damage:number) =>{
+        this.health -= damage;
+    }
+
+   
+
+
+
+    
+
 }
